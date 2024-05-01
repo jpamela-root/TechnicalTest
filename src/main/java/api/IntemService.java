0e61dca00747471606ec4c36c7a4c2c1189799ca
jpâmela_root <59771746+jpamela-root@users.noreply.github.com>
2024-05-01 13:34:17 -0300

    Update IntemService.java

--- src/main/java/api/IntemService.java
+++ src/main/java/api/IntemService.java
@@ -1,4 +1,26 @@
 package api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
 public class IntemService {
+
+    @Value("${wiremock.url}")
+    private String wiremockUrl;
+
+    private final RestTemplate restTemplate;
+
+    public IntemService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<Item> getItems() {
+        String url = wiremockUrl + "/items";
+        List forObject = restTemplate.getForObject(url, List.class);
+        return forObject;
+    }
 }
