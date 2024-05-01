50ea12fc9bf47d9bbe66d52fe1ecfcf56d30cdc3
jpâmela_root <59771746+jpamela-root@users.noreply.github.com>
2024-05-01 13:35:29 -0300

    Update PromotionService.java

--- src/main/java/api/PromotionService.java
+++ src/main/java/api/PromotionService.java
@@ -1,4 +1,33 @@
 package api;
 
+import java.util.List;
+import java.util.List;
+
 public class PromotionService {
+
+    public int calculateDiscount(List<Item> items) {
+        int appleCount = 0;
+        int orangeCount = 0;
+        int bananaCount = 0;
+
+        for (Item item : items) {
+            switch (item.getName()) {
+                case "apple":
+                    appleCount++;
+                    break;
+                case "orange":
+                    orangeCount++;
+                    break;
+                case "banana":
+                    bananaCount++;
+                    break;
+            }
+        }
+
+        int appleDiscount = (appleCount / 2) * 100;
+        int orangeDiscount = (orangeCount / 3) * 80;
+        int bananaDiscount = (bananaCount / 2) * 50;
+
+        return appleDiscount + orangeDiscount + bananaDiscount;
+    }
 }
