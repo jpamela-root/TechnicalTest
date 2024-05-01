4cc9b63eef161356927de8dc9c96b946cf10837c
jpâmela_root <59771746+jpamela-root@users.noreply.github.com>
2024-05-01 13:39:54 -0300

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,48 @@
+package api;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MainTest {
+
+    private static WireMockServer wireMockServer;
+
+    @BeforeAll
+    public static void setUp() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().port(8089));
+        wireMockServer.start();
+        WireMock.configureFor("localhost", 8089);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    public void testMain() {
+        // Given
+        PaymentService paymentService = new PaymentService(new PromotionService());
+        IntemService itemService = new IntemService(new RestTemplateBuilder().build());
+
+        // When
+        List<Item> items = itemService.getItems();
+        assertNotNull(items);
+        for (Item item : items) {
+            paymentService.addItem(item);
+        }
+
+        // Then
+        assertEquals(810, paymentService.getTotal());
+        assertEquals(500, paymentService.getTotalWithPromotions());
+        assertEquals(310, paymentService.getDiscount());
+    }
 }
+
